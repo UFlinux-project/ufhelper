3d4296281275fde78d6d2536c6f8a7a976ee231e
panmourovaty <35844848+panmourovaty@users.noreply.github.com>
2022-01-09 19:17:48 +0100

    replace debian package by flatpak

--- usr/share/ufhelper/functions/hardware/hardwarefunctions/rgb.sh
+++ usr/share/ufhelper/functions/hardware/hardwarefunctions/rgb.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
-    gnome-terminal --title="Installing OpenRGB" -- bash -c "sudo apt install openrgb; zenity --info --title='Install OpenRGB' --text='Task is completed'"  
+gnome-terminal --title="Installing OpenRGB" -- bash -c "flatpak install org.openrgb.OpenRGB --assumeyes; zenity --info --title='Install OpenRGB' --text='Task is completed'"  
 
